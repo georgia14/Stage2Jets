4af6f006dd9f2d2671cccc637a158fa353e28197
Georgia <georgia.karapostoli@gmail.com>
2015-03-18 12:19:51 +0100

    changes to match L1 calo tower new container

--- Stage2JetProducer/python/Stage2JetProducer_cfi.py
+++ Stage2JetProducer/python/Stage2JetProducer_cfi.py
@@ -5,10 +5,11 @@ Stage2JetProducer=cms.EDProducer(
     "Stage2JetProducer",
 
     #Trigger tower input
+    towerToken = cms.InputTag("caloStage2Digis", "MP"), # Added to make compatible with emulator
     towerInput = cms.InputTag("L1CaloTowerProducer"),
     mhtThreshold = cms.double(30),
     htThreshold = cms.double(30),
-    jetEtaCut = cms.double(3.0),
+    jetEtaCut = cms.int32(28)
 
 
     )
